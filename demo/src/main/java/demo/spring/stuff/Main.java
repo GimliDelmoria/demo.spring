042686223f27f3c268f10e5d54af04e0be5fdc06
GimliDelmoria <somewhereinto@thesea.com>
2025-02-02 14:24:10 +0100

    Renommer context en simplement ApplicationContext

--- demo/src/main/java/demo/spring/stuff/Main.java
+++ demo/src/main/java/demo/spring/stuff/Main.java
@@ -1,5 +1,6 @@
 package demo.spring.stuff;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.stream.IntStream;
@@ -8,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         // la première ligne donne :
         // org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'demo.spring.stuff.ICalculator' available: expected single matching bean but found 2: classicCalculator,supremeCalculator
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MaConfiguration.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(MaConfiguration.class);
         IntStream i1 = IntStream.of(1, 2, 3);
         IntStream i2 = IntStream.of(1, 2, 3);
         IComplexProcedure p = context.getBean(IComplexProcedure.class);
