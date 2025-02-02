2068db67bfd83f1eb2038bc8dd19560fb5cdb4a9
GimliDelmoria <somewhereinto@thesea.com>
2025-02-02 18:02:05 +0100

    Injection niveau méthode @Autowired

--- demo/src/main/java/demo/spring/stuff/ComplexProcedure.java
+++ demo/src/main/java/demo/spring/stuff/ComplexProcedure.java
@@ -8,18 +8,25 @@ import java.util.stream.IntStream;
 @Component
 public class ComplexProcedure implements IComplexProcedure {
 
-    // Fonctionne y compris sur les private members
+    // Fonctionne y compris sur les private members, à ne pas privilégier (moins simple après pour les tests)
     @Autowired
     private ICalculator calculator = null;
     @Autowired
     private IAggregator aggregator = null;
-    @Autowired
+
     private Limiter limiter = null;
 
     @Override
     public double work(IntStream i1, IntStream i2) {
         double d = aggregator.aggregate(calculator.compute(i1), calculator.compute(i2));
         return limiter != null ? limiter.limit(d) : d;
+    }
+
+    // injection par méthode
+    // appelée automatiquement à la construction du bean
+    @Autowired
+    public void setLimiter(Limiter limiter) {
+        this.limiter = limiter;
     }
 
 }
