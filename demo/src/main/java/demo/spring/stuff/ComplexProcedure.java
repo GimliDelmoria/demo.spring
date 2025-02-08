5b7f8ed465c4bdf84c7aa3e9ddbac1eceaff6d2a
GimliDelmoria <somewhereinto@thesea.com>
2025-02-08 14:16:11 +0100

    La notion de profile

--- demo/src/main/java/demo/spring/stuff/ComplexProcedure.java
+++ demo/src/main/java/demo/spring/stuff/ComplexProcedure.java
@@ -2,12 +2,18 @@ package demo.spring.stuff;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.IntStream;
 
 @Component
+@Profile("demo")
 @Lazy
+/**
+ * On place ce bean dans un profile. L'utilisation profile pourrait tout aussi bien
+ * être associée à une classe de @Configuration, ou au niveau d'un bean
+ */
 public class ComplexProcedure implements IComplexProcedure {
 
     @Autowired
--- demo/src/main/java/demo/spring/stuff/Main.java
+++ demo/src/main/java/demo/spring/stuff/Main.java
@@ -11,7 +11,10 @@ public class Main {
         // ComplexProcedure est lazy est n'existe pas encore
         IntStream i1 = IntStream.of(1, 2, 3);
         IntStream i2 = IntStream.of(1, 2, 3);
-        IComplexProcedure p = context.getBean(IComplexProcedure.class); // créé ici
+        // n'est plus créé car appartient au profile demo
+        // et on ne lance pas le programme avec ce profile.
+        // on indique aucun profile même en fait
+        IComplexProcedure p = context.getBean(IComplexProcedure.class);
         double d = p.work(i1, i2);
         System.out.println(d);
     }
