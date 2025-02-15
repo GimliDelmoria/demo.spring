ce337f30490632fdc670fc4a7749c856787fda3e
GimliDelmoria <somewhereinto@thesea.com>
2025-02-15 18:45:20 +0100

    Test

--- demo/src/main/java/demo/spring/stuff/Aggregator.java
+++ demo/src/main/java/demo/spring/stuff/Aggregator.java
@@ -3,7 +3,7 @@ package demo.spring.stuff;
 import org.springframework.stereotype.Repository;
 
 @Repository // décompilesr l'annotation pour voir qu'elle ne diffère pas vraiment d'un @Component classique
-// on pourrait aussi tout aussi bien définir notre propre annotation dedede
+// on pourrait aussi tout aussi bien définir notre propre annotation
 public class Aggregator implements IAggregator {
 
     @Override
